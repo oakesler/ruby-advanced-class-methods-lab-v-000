a360ede517dc28fb9690bf08b0b49ae8a4e5898b
Olivia Kesler <oakesler@protonmail.ch>
2019-08-07 18:33:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -60,6 +60,15 @@ class Song
     song.artist_name = "#{new_array[0]}".gsub(" ",'')
     song
   end
+  
+  def self.create_from_filename(string)
+    new_array = string.split("- ")
+    song_name = "#{new_array[1]}".gsub(".mp3",'')
+    song = self.new 
+    song.name = "#{song_name}"
+    song.artist_name = "#{new_array[0]}".gsub(" ",'')
+    song
+    
 end
     
     
